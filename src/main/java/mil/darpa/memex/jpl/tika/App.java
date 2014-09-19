35126a2b914668b19df5795c050c46033b502bf6
Lewis John McGibbney <lewis.j.mcgibbney@jpl.nasa.gov>
2014-09-19 12:26:47 -0400

    Add in phone parser app

--- src/main/java/mil/darpa/memex/jpl/tika/App.java
+++ src/main/java/mil/darpa/memex/jpl/tika/App.java
@@ -1,10 +1,22 @@
 package mil.darpa.memex.jpl.tika;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.tika.Tika;
 import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.CompositeParser;
 import org.apache.tika.parser.Parser;
+
 import mil.darpa.memex.jpl.tika.parser.*;
 
 /**
@@ -14,17 +26,57 @@ import mil.darpa.memex.jpl.tika.parser.*;
  */
 public class App 
 {
-	
-	private static HashMap<MediaType,Parser> parserMap = new HashMap<MediaType,Parser>();
-	
-    public static void main( String[] args )
-    {
-    	// load parsers (TBD: optionally from arg list)
-    	parserMap.put(MediaType.TEXT_HTML, new PhoneParser());
-    	
-        CompositeParser compositeParser = new CompositeParser();
-        compositeParser.setParsers(parserMap);
-        
-        
+
+  private static HashMap<MediaType,Parser> parserMap = new HashMap<MediaType,Parser>();
+
+  public static void main( String[] args ) throws IOException{
+    //Define options
+    @SuppressWarnings("static-access")
+    Option input = OptionBuilder.withLongOpt("input_file")
+    .withDescription("The input file you wish to parse").hasArg().withArgName("INPUT").create();
+    @SuppressWarnings("static-access")
+    Option output = OptionBuilder.withLongOpt("output_dir")
+    .withDescription("The output directory you wish to write to").hasArg().withArgName("OUTPUT").create();
+    Options opts = new Options();
+    opts.addOption(input);
+    opts.addOption(output);
+
+    // create the parser
+    GnuParser parser = new GnuParser();
+    CommandLine line = null;
+    HelpFormatter formatter = new HelpFormatter();
+    try {
+      if (args == null || args.length != 2) {
+        formatter.printHelp( "PhoneNumberParser", opts, true );
+        System.exit(0);;
+      } else {
+        // parse the command line arguments
+        line = parser.parse( opts, args );
+      }
     }
+    catch( ParseException exp ) {
+      // oops, something went wrong
+      System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+      formatter.printHelp( "PhoneNumberParser", opts, true );
+    }
+
+    Tika tika = new Tika();
+    
+    if (line.hasOption("input_file")) {
+      try {
+        String parseResult = tika.parse((new File(line.getOptionValue("input_file"))));
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+      }
+    } else {
+
+    }
+    // load parsers (TBD: optionally from arg list)
+    parserMap.put(MediaType.TEXT_HTML, new PhoneParser());
+
+    CompositeParser compositeParser = new CompositeParser();
+    compositeParser.setParsers(parserMap);
+
+
+  }
 }
--- src/main/java/mil/darpa/memex/jpl/tika/parser/PhoneParser.java
+++ src/main/java/mil/darpa/memex/jpl/tika/parser/PhoneParser.java
@@ -1,5 +1,7 @@
 package mil.darpa.memex.jpl.tika.parser;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
@@ -7,6 +9,8 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.io.IOUtils;
 import org.apache.tika.metadata.Metadata;
@@ -15,6 +19,10 @@ import org.apache.tika.parser.AbstractParser;
 import org.apache.tika.parser.ParseContext;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
 
 /**
  * 
@@ -27,28 +35,33 @@ import org.xml.sax.SAXException;
  */
 public class PhoneParser extends AbstractParser {
 
-    private static final Set<MediaType> SUPPORTED_TYPES =
-        Collections.unmodifiableSet(new HashSet<MediaType>(Arrays.asList(
-                MediaType.text("html"),
-                MediaType.application("xhtml+xml"),
-                MediaType.application("vnd.wap.xhtml+xml"),
-                MediaType.application("x-asp"))));	
-	
-	public Set<MediaType> getSupportedTypes(ParseContext context) {
-		return SUPPORTED_TYPES;
-	}
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  
+  private static final Set<MediaType> SUPPORTED_TYPES =
+      Collections.unmodifiableSet(new HashSet<MediaType>(Arrays.asList(
+          MediaType.text("html"),
+          MediaType.application("xhtml+xml"),
+          MediaType.application("vnd.wap.xhtml+xml"),
+          MediaType.application("x-asp"))));	
 
-	public void parse(InputStream stream, ContentHandler handler,
-			Metadata metadata, ParseContext context) throws IOException,
-			SAXException, TikaException {
-		String content = IOUtils.toString(stream);
-		
-		// call USC ISI de-obfuscating code transformations
-		// ...
-		
-		// call Google Phone Number extraction library
-		// ...
-		
-	}
+  public Set<MediaType> getSupportedTypes(ParseContext context) {
+    return SUPPORTED_TYPES;
+  }
 
+  public void parse(InputStream stream, ContentHandler handler,
+      Metadata metadata, ParseContext context) throws IOException,
+      SAXException, TikaException {
+    PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+    System.out.println("Supported Dialing Regions: " + StringUtils.join(phoneUtil.getSupportedRegions(), ", "));
+    System.out.println("Supported Global Network Calling Codes: " + StringUtils.join(phoneUtil.getSupportedGlobalNetworkCallingCodes(), ", "));
+    try {
+      PhoneNumber numberProto = phoneUtil.parse(parse, "US");
+    } catch (NumberParseException e) {
+      System.err.println("NumberParseException was thrown: " + e.toString());
+    }
+
+  }
 }
